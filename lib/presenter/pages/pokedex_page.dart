36f1d1b6c063d8ba131dbc096d4729fc523e5b3c
rodrigo <rodrigoasilvacc@gmail.com>
2023-01-19 11:49:08 -0300

    Fix Imports

--- lib/presenter/pages/pokedex_page.dart
+++ lib/presenter/pages/pokedex_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+
 import 'package:pokemon_consome_api/presenter/components/pokedex_app_bar.dart';
 import 'package:pokemon_consome_api/presenter/components/pokemon_list_tile.dart';
 import 'package:pokemon_consome_api/presenter/controller/http.dart';
+
+import '../../domain/entity/pokemon.dart';
 
 class PokeDexPage extends StatefulWidget {
   PokeDexPage({Key? key}) : super(key: key);
@@ -28,14 +31,42 @@ class _PokeDexPageState extends State<PokeDexPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PokeDexAppBar(),
-      body: ListView.builder(
-        itemCount: pokemon?.length ?? 0,
-        itemBuilder: (context, index) => PokemonListTile(
-            type: pokemon![index]['type'],
-            imgUrl: pokemon![index]['img'],
-            name: pokemon![index]['name'],
-            number: pokemon![index]['num']),
-      ),
+      body: FutureBuilder<List<Pokemon>>(
+          future: controllerHttp.fetchPokemonData(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.green,
+                ),
+              );
+            }
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) => PokemonListTile(
+                  type: snapshot.data![index].type!,
+                  imgUrl: snapshot.data![index].img!,
+                  name: snapshot.data![index].name!,
+                  number: snapshot.data![index].num!),
+            );
+          }),
     );
+  }
+
+  void _onChangedSearchList(String query) {
+    List listResult = [];
+    List foundResult = [];
+
+    if (query.isEmpty) {
+      listResult = pokemon!;
+    } else {
+      listResult = pokemon!
+          .where((element) =>
+              element["name"].toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    }
+    setState(() {
+      foundResult = listResult;
+    });
   }
 }
